8cc720b3393e403556832d48ecd69dcf2d0a9d25
goav550 <goav550@gmail.com>
2025-01-08 17:23:44 +0900

    [refactor] refactor 완성

--- lib/ui/pages/todo/memo/todo_memo_page.dart
+++ lib/ui/pages/todo/memo/todo_memo_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:planit/ui/pages/todo/memo/widgets/todo_memo_body.dart';
 
 class TodoMemoPage extends StatelessWidget {
   TodoMemoPage({super.key});
@@ -7,24 +8,7 @@ class TodoMemoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: ListView(
-          children: [
-            Text(
-              "제목",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            TextField(
-              maxLines: null,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "메모 추가",
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: TodoMemoBody(),
     );
   }
 }

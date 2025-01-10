39b2567ae831a02ab2a0c47db6485b09cb5539d7
goav550 <goav550@gmail.com>
2025-01-10 17:51:16 +0900

    [fix] bug

--- lib/data/model/user.dart
+++ lib/data/model/user.dart
@@ -1,15 +1,8 @@
 class User {
-<<<<<<< HEAD
   int? id;
   String? username;
 
   User.fromMap(Map<String, dynamic> map)
       : this.id = map["id"],
         this.username = map["username"];
-=======
-  int id;
-  String username;
-
-  User(this.id, this.username);
->>>>>>> 0ed494c ([feat] ë¡회원가입 구현)
 }
